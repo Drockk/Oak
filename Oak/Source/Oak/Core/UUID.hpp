bcb70e66155fba36ca1d8ecd78be9e0478802410
Bartosz Zielonka <batziell@gmail.com>
2023-07-08 09:39:17 +0200

    Added UUID

---
+++ Oak/Source/Oak/Core/UUID.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace oak
+{
+    class UUID
+    {
+    public:
+        UUID();
+        ~UUID() = default;
+        UUID(uint64_t t_uuid);
+        UUID(const UUID&) = default;
+
+        operator uint64_t() const
+        {
+            return m_UUID;
+        }
+
+    private:
+        uint64_t m_UUID;
+    };
+}
+
+namespace std
+{
+    template<typename T> struct hash;
+
+    template<>
+    struct hash<oak::UUID>
+    {
+        std::size_t operator()(const oak::UUID& t_uuid) const
+        {
+            return static_cast<uint64_t>(t_uuid);
+        }
+    };
+}
