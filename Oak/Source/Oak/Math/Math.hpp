4a761da888e1637a892512a944b335f792f253d5
Bartosz Zielonka <batziell@gmail.com>
2023-07-13 17:09:11 +0200

    Added Math module

---
+++ Oak/Source/Oak/Math/Math.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace oak::math
+{
+    bool decomposeTransform(const glm::mat4& t_transform, glm::vec3& t_translation, glm::vec3& t_rotation, glm::vec3& t_scale);
+}
