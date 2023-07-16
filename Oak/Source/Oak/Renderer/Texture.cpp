c2608da413b93a2c34fdd784032017043e544f7c
Bartosz Zielonka <batziell@gmail.com>
2023-07-16 09:49:54 +0200

    Added texture class

---
+++ Oak/Source/Oak/Renderer/Texture.cpp
@@ -0,0 +1,38 @@
+#include "oakpch.hpp"
+#include "Oak/Renderer/Texture.hpp"
+
+#include "Oak/Renderer/Renderer.hpp"
+#include "Platform/OpenGL/OpenGLTexture.hpp"
+
+namespace oak
+{
+    Ref<Texture2D> Texture2D::create(const TextureSpecification& t_specification)
+    {
+        switch (Renderer::getAPI())
+        {
+        case RendererAPI::API::None:
+            OAK_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return createRef<openGL::Texture2D>(t_specification);
+        }
+
+        OAK_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+
+    Ref<Texture2D> Texture2D::create(const std::string& t_path)
+    {
+        switch (Renderer::getAPI())
+        {
+        case RendererAPI::API::None:
+            OAK_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return createRef<OpenGLTexture2D>(t_path);
+        }
+
+        OAK_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+}
---
+++ Oak/Source/Oak/Renderer/Texture.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Oak/Core/Base.hpp"
+
+#include <string>
+
+namespace oak
+{
+    enum class ImageFormat
+    {
+        None = 0,
+        R8,
+        RGB8,
+        RGBA8,
+        RGBA32F
+    };
+
+    struct TextureSpecification
+    {
+        uint32_t Width = 1;
+        uint32_t Height = 1;
+        ImageFormat Format = ImageFormat::RGBA8;
+        bool GenerateMips = true;
+    };
+
+    class Texture
+    {
+    public:
+        virtual ~Texture() = default;
+
+        virtual const TextureSpecification& getSpecification() const = 0;
+
+        virtual uint32_t getSize() const = 0;
+        virtual uint32_t getWidth() const = 0;
+        virtual uint32_t getHeight() const = 0;
+        virtual uint32_t getRendererID() const = 0;
+
+        virtual const std::string& getPath() const = 0;
+
+        virtual void setData(void* data, uint32_t size) = 0;
+
+        virtual void bind(uint32_t slot = 0) const = 0;
+
+        virtual bool isLoaded() const = 0;
+
+        virtual bool operator==(const Texture& other) const = 0;
+    };
+
+    class Texture2D : public Texture
+    {
+    public:
+        static Ref<Texture2D> create(const TextureSpecification& specification);
+        static Ref<Texture2D> create(const std::string& path);
+    };
+
+}
